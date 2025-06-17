e2c62e05dc6664f8734626618abb91ab0ada563c
Loara228 <my.zero.ife@gmail.com>
2025-06-17 23:44:28 +0500

    jashdkasd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    
+    let server = HttpServer::new(move || {
+        App::new()
+            .service(actix_files::Files::new("/", "src/static").show_files_listing())
+        
+    });
+
+    server
+        .bind(("127.0.0.1", 8080))
+        .unwrap()
+        .run()
+        .await
 }
