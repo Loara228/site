405af7494b438ca495f0d2e9831dc842a0006a75
Loara228 <my.zero.ife@gmail.com>
2025-06-18 15:46:54 +0500

    a

--- readme.md
+++ readme.md
@@ -1,8 +1,9 @@
 
-# Site
+<details>
 
-TODO:
-   - about (index.html)
-   - switch
-   - about me blog
-   - sandbox
+<summary>Windows</summary>
+
+[Rust](https://www.rust-lang.org/tools/install) - desc.<br>
+[Postgres](https://www.enterprisedb.com/downloads/postgres-postgresql-downloads) - desc.
+
+</details>
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+pub struct Cli
+{
+    #[arg(short, long)]
+    pub address: Option<String>,
+
+    #[arg(short, long)]
+    pub port: Option<u16>
+}
+
+pub struct AppData {
+    
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let cli = Cli::parse();
+
+    let addr: &str = &cli.address.unwrap_or("127.0.0.1".to_string());
+    let port: u16 = cli.port.unwrap_or(8080u16);
     
     let server = HttpServer::new(move || {
         App::new()
@@ -11,7 +30,7 @@ async fn main() -> std::io::Result<()> {
     });
 
     server
-        .bind(("127.0.0.1", 8080))
+        .bind((addr, port))
         .unwrap()
         .run()
         .await
